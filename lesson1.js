1f1605c3b0fc1d78ac86b1a26c20c034a714296b
Dima <88742126+DimaWeb44@users.noreply.github.com>
2021-11-09 21:56:47 +0300

    Add files via upload

---
+++ lesson1.js
@@ -0,0 +1,12 @@
+function yourFutureCareer(){
+    var career = Math.random();
+      if (career <= 0.32) {
+        return "FrontEnd Developer"}
+       else if (career <= 0.65) {
+        return "BackEnd Developer"}
+       else {
+        return "Full-Stack Developer"
+      }
+      }
+  
+  
