31336f0dfd6df6010a3fb53f376348ce0a2fd70d
Dima <88742126+DimaWeb44@users.noreply.github.com>
2021-11-12 23:27:39 +0300

    Add files via upload

---
+++ lesson28.js
@@ -0,0 +1,11 @@
+function mergeArrays(a, b) {
+  const maxLength = Math.max(a.length, b.length);
+  let result = [];
+
+  for (let i = 0; i < maxLength; i++) {
+    result.push(a[i]);
+    result.push(b[i]);
+  }
+
+  return result.filter((value) => value !== undefined);
+}
