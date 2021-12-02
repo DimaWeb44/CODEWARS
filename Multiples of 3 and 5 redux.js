ee7a7f417b4463d3bf4880fa4ca1e62a0c12692c
Dima <88742126+DimaWeb44@users.noreply.github.com>
2021-12-02 20:49:21 +0300

    Create Multiples of 3 and 5 redux.js

---
+++ Multiples of 3 and 5 redux.js
@@ -0,0 +1,8 @@
+function solution(number){
+  let arr=[];
+    for (let i=number-1;i>=0;i--){
+      if (i%3===0) arr.push(i);
+      if (i%5===0) arr.push(i);
+}
+  return [...new Set(arr)].reduce((a,b)=>a+b,0)
+}
