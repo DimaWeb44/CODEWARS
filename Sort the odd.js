d9dc53c4be969a87738e1bac698aff14a6b2d529
Dima <88742126+DimaWeb44@users.noreply.github.com>
2022-09-13 21:22:20 +0300

    Create Sort the odd.js

---
+++ Sort the odd.js
@@ -0,0 +1,8 @@
+function sortArray(array) {
+let indices = [];
+array
+    .filter((v, i) => v % 2 && indices.push(i))
+    .sort((a, b) => a - b)
+    .forEach((v, i) => array[indices[i]] = v);
+  return array
+}
