81431be9d07cf08779ead4e58f2cc9c1b51bac1b
Dima <88742126+DimaWeb44@users.noreply.github.com>
2022-09-22 23:06:41 +0300

    Create The Coupon Code.js

---
+++ The Coupon Code.js
@@ -0,0 +1,8 @@
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+  let date = new Date(currentDate);
+  let expDate = new Date(expirationDate);
+
+  if (date.getTime() - expDate.getTime() > 0) return false;   
+  if (enteredCode !== correctCode) return false;
+  return true;
+}
